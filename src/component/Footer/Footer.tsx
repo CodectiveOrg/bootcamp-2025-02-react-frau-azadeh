025c53272866d33a8ed23b1d0e390e4b381b5da6
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-25 06:19:02 -0800

    add Footer

---
+++ src/component/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+import styles from "./Footer.module.css";
+export function Footer () :ReactElement {
+  return (
+    <div className={styles.footer}>
+      footer
+    </div>
+  )
+}
