887b5a9872bd7ff7a482375a02fe15d0e661969b
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-25 06:19:35 -0800

    add RootLayout

---
+++ src/layout/RootLayout.tsx
@@ -0,0 +1,15 @@
+import { Header } from '../component/Header/Header'
+import { Outlet } from 'react-router'
+import { Footer } from '../component/Footer/Footer'
+
+export const RoorLayout = () => {
+  return (
+    <>
+        <Header/>
+        <main>
+            <Outlet/>
+        </main>
+        <Footer/>
+    </>
+  )
+}
