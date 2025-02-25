ca66bcaeea52ded33b1f1c8be83d177d26c5eaa2
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-25 06:19:21 -0800

    add Header

---
+++ src/component/Header/Header.tsx
@@ -0,0 +1,28 @@
+import { ReactElement } from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+
+type NavItem ={
+  title: string;
+  href: string;
+}
+const navItems: NavItem[] =[
+  {title:"خانه", href: "/"},
+  {title:"درباره ما", href:"/about"},
+
+];
+export function Header(): ReactElement{
+  return(
+    <div className={styles.header}>
+        <nav>
+          <ul>
+            {navItems.map((item)=>(
+              <li key={item.title}>
+                <Link to={item.href} >{item.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+    </div>
+  )
+}
